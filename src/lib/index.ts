0464df6ebce04c0f13041397bba3fb776e112889
Marc Klinger <mklinger@mklinger.de>
2023-10-05 10:36:56 +0200

    Export Button and Icon

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,5 @@ export * from "./theme";
 export * from "./components/Slideshow";
 export * from "./components/QuickStartSlideshow";
 export * from "./components/UnmanagedSlideshow";
+export { default as Icon } from "./components/controls/Icon"
+export { default as Button } from "./components/controls/Button"
