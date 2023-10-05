fc192d0c2a3b8e9ec35619c02e188457647ab067
Marc Klinger <mklinger@mklinger.de>
2023-10-05 09:41:53 +0200

    Add types

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { resolve } from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      include: "./src/lib/",
+      rollupTypes: true
+    })
+  ],
   build: {
     lib: {
       entry: resolve(__dirname, "./src/lib/index.ts"),
