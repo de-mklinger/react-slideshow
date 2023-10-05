76bbc69141887ef9926df750c01781a10409c3f8
Marc Klinger <mklinger@mklinger.de>
2023-10-05 10:37:03 +0200

    Improve demo

--- src/demo/App.tsx
+++ src/demo/App.tsx
@@ -1,4 +1,4 @@
-import { QuickStartSlideshow, TopBarChildren } from "../lib";
+import {QuickStartSlideshow, TopBarChildren, Icon} from "../lib";
 import { customizedDemoItems, CustomizedSlideshowItem } from "./demo-items.ts";
 
 export default function App() {
@@ -6,7 +6,11 @@ export default function App() {
     const item = props.items[props.itemIdx];
 
     const licenseLabel = (
-      <span title={`Image License: ${item.license}`}>[i]</span>
+      <Icon title={`Image License: ${item.license}`}>
+        <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+          <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M8 9h2v5m-2 0h4M9.408 5.5h.01M19 10a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/>
+        </svg>
+      </Icon>
     );
 
     return (
--- src/demo/demo-items.ts
+++ src/demo/demo-items.ts
@@ -1,26 +1,11 @@
 import { SlideshowItem } from "../lib";
 
-export const xDemoItems: SlideshowItem[] = [
-  {
-    id: "1",
-    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Pillars_of_Creation_%28NIRCam_Image%29.jpg/1182px-Pillars_of_Creation_%28NIRCam_Image%29.jpg",
-    thumbnailUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Pillars_of_Creation_%28NIRCam_Image%29.jpg/277px-Pillars_of_Creation_%28NIRCam_Image%29.jpg",
-  },
-  {
-    id: "2",
-    url: "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Holy_SURP_Hovhannes_Church.jpg/1280px-Holy_SURP_Hovhannes_Church.jpg",
-    thumbnailUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Holy_SURP_Hovhannes_Church.jpg/320px-Holy_SURP_Hovhannes_Church.jpg",
-  },
-];
-
 export type CustomizedSlideshowItem = SlideshowItem & {
-  description?: string,
-  caption?: string,
-  license: string,
-  infoUrl?: string,
-}
+  description?: string;
+  caption?: string;
+  license: string;
+  infoUrl?: string;
+};
 
 export const customizedDemoItems: CustomizedSlideshowItem[] = [
   {
@@ -29,9 +14,11 @@ export const customizedDemoItems: CustomizedSlideshowItem[] = [
     thumbnailUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f1/Pillars_of_Creation_%28NIRCam_Image%29.jpg/277px-Pillars_of_Creation_%28NIRCam_Image%29.jpg",
     description: "Young stars, James Webb Space Telescope",
-    caption: "Young stars form in 'The Pillars of Creation' as seen by NASA’s James Webb Space Telescope’s near-infrared camera",
+    caption:
+      "Young stars form in 'The Pillars of Creation' as seen by NASA’s James Webb Space Telescope’s near-infrared camera",
     license: "Public domain",
-    infoUrl: "https://commons.wikimedia.org/wiki/File:Pillars_of_Creation_(NIRCam_Image).jpg"
+    infoUrl:
+      "https://commons.wikimedia.org/wiki/File:Pillars_of_Creation_(NIRCam_Image).jpg",
   },
   {
     id: "2",
@@ -39,7 +26,52 @@ export const customizedDemoItems: CustomizedSlideshowItem[] = [
     thumbnailUrl:
       "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Holy_SURP_Hovhannes_Church.jpg/320px-Holy_SURP_Hovhannes_Church.jpg",
     license: "CC BY-SA 4.0 DEED",
-    infoUrl: "https://commons.wikimedia.org/wiki/File:Holy_SURP_Hovhannes_Church.jpg"
+    infoUrl:
+      "https://commons.wikimedia.org/wiki/File:Holy_SURP_Hovhannes_Church.jpg",
   },
+  {
+    id: "3",
+    url: "https://upload.wikimedia.org/wikipedia/commons/0/01/Alcedo_atthis_-_Riserve_naturali_e_aree_contigue_della_fascia_fluviale_del_Po.jpg",
+    thumbnailUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Alcedo_atthis_-_Riserve_naturali_e_aree_contigue_della_fascia_fluviale_del_Po.jpg/320px-Alcedo_atthis_-_Riserve_naturali_e_aree_contigue_della_fascia_fluviale_del_Po.jpg",
+    license: "CC BY-SA 4.0 DEED",
+    infoUrl:
+      "https://commons.wikimedia.org/wiki/File:Alcedo_atthis_-_Riserve_naturali_e_aree_contigue_della_fascia_fluviale_del_Po.jpg",
+  },
+  {
+    id: "4",
+    url: "https://upload.wikimedia.org/wikipedia/commons/8/86/Evolution_of_a_Tornado.jpg",
+    thumbnailUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Evolution_of_a_Tornado.jpg/320px-Evolution_of_a_Tornado.jpg",
+    license: "CC BY-SA 4.0 DEED",
+    infoUrl:
+      "https://commons.wikimedia.org/wiki/File:Evolution_of_a_Tornado.jpg",
+  },
+  {
+    id: "5",
+    url: "https://upload.wikimedia.org/wikipedia/commons/e/ea/FCAB_EMD_GT22CU-3_San_Pedro_-_Ascotan.jpg",
+    thumbnailUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/FCAB_EMD_GT22CU-3_San_Pedro_-_Ascotan.jpg/640px-FCAB_EMD_GT22CU-3_San_Pedro_-_Ascotan.jpg",
+    license: "CC BY-SA 4.0 DEED",
+    infoUrl:
+      "https://commons.wikimedia.org/wiki/File:FCAB_EMD_GT22CU-3_San_Pedro_-_Ascotan.jpg",
+  },
+  {
+    id: "6",
+    url: "https://upload.wikimedia.org/wikipedia/commons/e/e8/Khaoyai_06.jpg",
+    thumbnailUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Khaoyai_06.jpg/320px-Khaoyai_06.jpg",
+    license: "CC BY-SA 3.0 DEED",
+    infoUrl: "https://commons.wikimedia.org/wiki/File:Khaoyai_06.jpg",
+    description:
+      "Wild elephants walking up a road in the area of Khao Yai National Park.",
+  },
+];
 
-]
+export const minimalDemoItems: SlideshowItem[] = customizedDemoItems.slice(0, 3).map(
+  (i) => ({
+    id: i.id,
+    url: i.url,
+    thumbnailUrl: i.thumbnailUrl,
+  }),
+);
